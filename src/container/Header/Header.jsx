3c8d01b03f272672847f7a1b3204e54ac3d9cd6e
Melvin Towo <melvintowo@gmail.com>
2022-05-24 18:01:42 -0500

    firts commit

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { images } from '../../constants'
+import './Header.scss'
+import { motion } from 'framer-motion'
+const Header = () => {
+  return (
+    <div className="app__header app__flex" >
+      <motion.div
+        whileInView={{x: [-100, 0], opcaity: [0, 1]}}
+        transition={{duration: 0.5}}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{marginLeft: 20}}>
+              <p className="p-text">Hello, I'm</p>
+              <h1 className="head-text">Melvin</h1>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  )
+}
+
+export default Header
